f3e08357bbd5977918c36089869c2f967b0badb8
Robert Balazsi <contact@robertbalazsi.com>
2021-01-03 01:29:12 +0200

    Added extra data in forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -2,7 +2,7 @@ const request = require('postman-request')
 
 
 const forecast = (longitude, latitude, callback) => {
-    const url = `http://api.weatherstack.com/current?access_key=8ceda2b43a2414cebbf14b4c5bf2a69f&query=${latitude},${longitude}&units=f`
+    const url = `http://api.weatherstack.com/current?access_key=8ceda2b43a2414cebbf14b4c5bf2a69f&query=${latitude},${longitude}&units=m`
 
     request({url, json: true}, (error, {body} = {}) => {
         if (error) {
@@ -11,7 +11,7 @@ const forecast = (longitude, latitude, callback) => {
             callback('Unable to find location')
         } else {
             const current = body.current
-            callback(undefined, `${current.weather_descriptions[0]}. It is currently ${current.temperature} degrees out. It feels like ${current.feelslike} degrees out.`)
+            callback(undefined, `${current.weather_descriptions[0]}. It is currently ${current.temperature} degrees out. It feels like ${current.feelslike} degrees out. The humidity is ${current.humidity}%.`)
         }
     })
 }
